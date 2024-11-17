62b0d4f8061f2a280795b02b1d72135baa5956dd
GonzaEC <gonzaloecheverriacrenna1@gmail.com>
2024-11-17 13:43:18 -0300

    .

--- PokerDeDados/src/com/edu/unlu/generala/modelos/Apuesta.java
+++ PokerDeDados/src/com/edu/unlu/generala/modelos/Apuesta.java
@@ -1,6 +1,28 @@
 package com.edu.unlu.generala.modelos;
 
 public class Apuesta {
-    private int cantidad;
     private Jugador apostador;
+    private int cantidad;
+
+    public Apuesta(Jugador jugador, int cantidad){
+        this.apostador = jugador;
+        this.cantidad = cantidad;
+    }
+
+    public Jugador getApostador() {
+        return apostador;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setApuesta(int cantidad){
+        this.cantidad = cantidad;
+    }
 }
+
+
+
+
+
+
--- PokerDeDados/src/com/edu/unlu/generala/modelos/Jugador.java
+++ PokerDeDados/src/com/edu/unlu/generala/modelos/Jugador.java
@@ -2,6 +2,22 @@ package com.edu.unlu.generala.modelos;
 
 public class Jugador {
     private String nombre;
-    private Apuesta apostado;
-    private int saldo;
+    private float saldo;
+
+    public Jugador(String nombre, float saldoInicial) {
+        this.nombre = nombre;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void agregarSaldo(float saldo){
+        this.saldo += saldo;
+    }
 }
