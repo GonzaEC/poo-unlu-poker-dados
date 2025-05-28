2f9a098b956494b214b2936a6eed9714912705ae
GonzaEC <gonzaloecheverriacrenna1@gmail.com>
2025-05-27 22:09:02 -0300

    asdasd

--- PokerDeDados/src/Main.java
+++ PokerDeDados/src/Main.java
@@ -1,7 +1,7 @@
 import com.edu.unlu.generala.controladores.ControladorGenerala;
 import com.edu.unlu.generala.vista.IVista;
 import com.edu.unlu.generala.vista.InterfazConsola;
-
+gu
 public class Main {
     public static void main(String[] args) {
         ControladorGenerala controlador = new ControladorGenerala();
